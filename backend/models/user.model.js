9dc6730b37ad022c418e0b0e14ba11056cf209d8
josh098741 <morongemokaya@gmail.com>
2025-10-16 13:29:46 +0300

    Additional commits to the User Model

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6,'Password must be atleast 6 characters Long']
+    },
+    cartItems: [
+        {
+            quantity: {
+                type: Number,
+                default: 1
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product'
+            }
+        }
+    ],
+    role: {
+        type: String,
+        enum: ["customer","admin"],
+        default: "customer"
+    }
+},{timestamps: true})
+
+const User = mongoose.model("User",userSchema)
+export default User
