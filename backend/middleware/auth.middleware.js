d18c7fc15a44243e920704079a7fce9eaf77430d
josh098741 <morongemokaya@gmail.com>
2025-10-16 20:48:28 +0300

    Addition of auth middleware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model.js'
+
+export const protectRoute = async (req, res, next) => {
+    try{
+        const accessToken = req.cookies.accessToken
+
+        if(!accessToken){
+            return res.status(401).json({ message: "Unathorizezd - No Token Provided" })
+        }
+
+        try{
+            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+            const user = await User.findById(decoded.userId).select("-password")
+
+            if(!user){
+                return res.status(401).json({ message: "User Not Found" })
+            }
+
+            req.user = user
+
+            next() 
+        }catch(error){
+
+            if(error.name === "TokenExpiredError"){
+                return res.status(401).json({ message: "Unathorized - Access token expired" })
+            }
+
+            throw error
+        }
+         
+
+        
+    }catch(error){
+        console.log("Error in Protect Route Middleware", error.message)
+        res.status(401).json({ message: "Unauthorized - Invalid access Token" })
+    }
+}
+
+export const adminRoute = async (req,res,next) => {
+    try{
+
+    }catch(error){
+        
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { getAllProducts } from '../controllers/product.controller'
+import { getAllProducts } from '../controllers/product.controller.js'
+import { protectRoute,adminRoute } from '../middleware/auth.middleware.js'
 
 const router = express()
 
