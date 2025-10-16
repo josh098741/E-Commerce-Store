bb198b12efc4f60ec3c4328bdcf4e55099526e3f
josh098741 <morongemokaya@gmail.com>
2025-10-16 15:09:20 +0300

    Additional commits

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,15 +9,19 @@ export const signup =  async (req,res) => {
         }
 
         const userExists = await User.findOne({ email })
+
         if(userExists){
             return res.status(400).json({message: "User Already exists"})
         }
+
         const user = await User.create({
             name,
             email,
             password
         })
+
         res.status(201).json({ user, message: "User created successfully" })
+        
     }catch(error){
         res.status(500).json({ message: "Internal server Error" })
     }
