30135440ef53efac60cea89f406d999c53685c1d
josh098741 <morongemokaya@gmail.com>
2025-10-16 16:25:43 +0300

    Successful implementation of Logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -80,5 +80,20 @@ export const login = async (req,res) => {
 }
 
 export const logout = async (req,res) => {
-    res.send("Logout route called")
+    try{
+        const refreshToken = req.cookies.refreshToken
+
+        if(refreshToken){
+            const decoded = jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET)
+            await redis.del(`refresh_token: ${decoded.userId}`)
+        }
+
+        res.clearCookie("accessToken")
+
+        res.clearCookie("refreshToken")
+
+        res.status(200).json({ message: "Logged out Successfully" })
+    }catch(error){
+        res.status(500).json({ message: "Internal Server Error", error: error.message })
+    }
 }
