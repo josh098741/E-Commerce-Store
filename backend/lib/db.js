0d7ce5e887be7fc6d8ae3fc128d6767b8fbb9951
josh098741 <morongemokaya@gmail.com>
2025-10-16 13:13:11 +0300

    Additional commits

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose'
 
 export const connectDB = async (req,res) => {
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        const connection = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Mongo DB conneected successfully: ${connection.connection.host}`)
     }catch(error){
-
+        console.error("Error in connecting to the database",error.message)
+        process.exit(1)
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -10,6 +11,15 @@ const PORT = process.env.PORT || 5000
 
 app.use("/api/auth",authRoutes)
 
-app.listen(PORT, () => {
-    console.log("Server is running on http://localhost:" + PORT)
-}) 
+const start = async () => {
+    try{
+        await connectDB()
+        app.listen(PORT, () => {
+            console.log("Server is running on http://localhost:" + PORT)
+        }) 
+    }catch(error){
+        console.log("Failed to connect to the server")
+    }
+}
+
+start()
