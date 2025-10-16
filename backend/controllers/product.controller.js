4af4f894aa79f15e59e8ff8f858033ed93f5dfbd
josh098741 <morongemokaya@gmail.com>
2025-10-16 23:33:24 +0300

    Additional commit in get recomendation

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -93,3 +93,26 @@ export const deleteProduct = async (req,res) => {
         res.status(500).json({ message: "Internal server error", error: error.message })
     }
 }
+
+export const getRecommendedProducts = async (req,res) => {
+    try{
+        const products = await Product.aggregate([
+            {
+                $sample: {size:3}
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    image: 1,
+                    price: 1
+                }
+            }
+        ])
+        res.status(200).json(products)
+    }catch(error){
+        console.log("Error in getRecommendation controller", error.meessage)
+        res.status(500).json({ message: "Internal Server Error", error: error.message })
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { getAllProducts,getFeaturedProducts,createProduct,deleteProduct } from '../controllers/product.controller.js'
+import { getAllProducts,getFeaturedProducts,createProduct,deleteProduct,getRecommendedProducts } from '../controllers/product.controller.js'
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js'
 
 const router = express()
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
 router.get("/featured", getFeaturedProducts)
+router.get("/recommendation", getRecommendedProducts)
 router.post("/", protectRoute, adminRoute, createProduct)
 router.delete("/:id",protectRoute, adminRoute, deleteProduct)
+
 
 export default router
