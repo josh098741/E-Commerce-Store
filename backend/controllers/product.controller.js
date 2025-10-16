f3c9e59ed81138f3a5dffd85e4670296871d786c
josh098741 <morongemokaya@gmail.com>
2025-10-16 21:28:12 +0300

    Additional commit to get featured products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js"
 import Product from "../models/product.model.js"
 
 export const getAllProducts = async (req,res) => {
@@ -9,5 +10,32 @@ export const getAllProducts = async (req,res) => {
     }catch(error){
         console.log("Error in get All products controller", error.message)
         res.status(500).json({ message: "Internal server Error", error: error.message })
+    }
+}
+
+export const getFeaturedProducts = async (req,res) => {
+    try{
+
+        let featuredProducts =  await redis.get("featured_products")
+
+        if(featuredProducts){
+            return res.status(200).json(JSON.parse(featuredProducts))
+        }
+
+        //if not in redis we should fetch it from mongo db
+        //.lean() is gonna return a plain javaScript object instead of a mongo db document which speeds perfomance
+        featuredProducts = await Product.find({ isFeatured:true }).lean()
+
+        if(!featuredProducts){
+            return res.status(404).json({ message: "No Featured products found" })
+        }
+
+        //Store in redis for future quick access
+        await redis.set("featured_products", JSON.stringify(featuredProducts))
+
+        res.status(200).json({ featuredProducts })
+    }catch(error){
+        console.log("Error in the get Featured Products controller", error.message)
+        res.status(500).json({message: "Internal Server Error"})
     }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getAllProducts } from '../controllers/product.controller.js'
+import { getAllProducts,getFeaturedProducts } from '../controllers/product.controller.js'
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js'
 
 const router = express()
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
+router.get("/featured", getFeaturedProducts)
 
 export default router
