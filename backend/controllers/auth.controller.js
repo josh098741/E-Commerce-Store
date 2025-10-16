5a4914722d2bae16a6b061fda7da02e6b7e108c4
josh098741 <morongemokaya@gmail.com>
2025-10-16 15:56:38 +0300

    Additional commit to auth controller and redis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,40 @@
+import { redis } from '../lib/redis.js'
 import User from '../models/user.model.js'
+import jwt from 'jsonwebtoken'
+
+const generateToken = (userId) => {
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: "15m",
+    })
+
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: "7d",
+    })
+
+    return {accessToken, refreshToken}
+}
+
+const storeRefreshToken = async (userId,refreshToken) => {
+    await redis.set(`refresh_token: ${userId}`,refreshToken,"EX",7*24*60*60)
+}
+
+const setCookies = (res,accessToken,refreshToken) => {
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, // prevent XSS attacks, cross site scripting
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevents CSRF attack cross-site request forgery
+        maxAge: 15 * 60 * 1000 //15 minutes
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+
+}
 
 export const signup =  async (req,res) => {
     try{
@@ -20,8 +56,14 @@ export const signup =  async (req,res) => {
             password
         })
 
+        //Authentication
+        const {accessToken, refreshToken} = generateToken(user._id)
+        await storeRefreshToken(user._id,refreshToken)
+
+        setCookies(res, accessToken, refreshToken)
+
         res.status(201).json({ user, message: "User created successfully" })
-        
+
     }catch(error){
         res.status(500).json({ message: "Internal server Error" })
     }
