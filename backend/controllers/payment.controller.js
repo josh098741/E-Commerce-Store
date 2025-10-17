da62b6c08925ec6bce62f7bec96133a177ed7fe7
josh098741 <morongemokaya@gmail.com>
2025-10-17 18:12:30 +0300

    Transfer of checkout success

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -65,6 +65,47 @@ export const createCheckoutSession = async (req,res) => {
     }
 }
 
+export const checkoutSuccess =  async(req,res) => {
+    try{
+        const { sessionId } = req.body
+        const session = await stripe.checkout.sessions.retrieve(sessionId)
+
+        if(session.payment_status === "paid"){
+            if(session.metadata.couponCode){
+                await Coupon.findOneAndUpdate({
+                    code: session.metadata.couponCode, 
+                    userId: session.metadata.userId
+                },{
+                    isActive: false
+                })
+            }
+
+            //create a new Order
+            const products = JSON.parse(session.metadata.products);
+            const newOrder = new Order({
+                user: session.metadata.userId,
+                products: products.map(product => ({
+                    product: product.id,
+                    quantity: product.quantity,
+                    price: product.price
+                })),
+                totalAmount: session.amount_total / 100, //Convert from cents to dollars
+                paymentIntent: session.payment_intent,
+                stripeSessionId: sessionId
+            })
+            await newOrder.save()
+            res.status(200).json({
+                success: true,
+                message: "Payment successfull, order created, and coupon deactivated if used",
+                orderId: newOrder._id
+            })
+        }
+    }catch(error){
+        console.error("Error processing successful checkout:",error)
+        res.status(500).json({ message: "Error in processing checkout",error: error.message })
+    }
+}
+
 async function createStripeCoupon(discountPercantage){
     const coupon = await stripe.coupons.create({
         percent_off: discountPercantage,
--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -8,45 +8,6 @@ import Order from '../models/order.model.js'
 const router = express.Router()
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession )
-router.post("/checkout-success", protectRoute, async(req,res) => {
-    try{
-        const { sessionId } = req.body
-        const session = await stripe.checkout.sessions.retrieve(sessionId)
-
-        if(session.payment_status === "paid"){
-            if(session.metadata.couponCode){
-                await Coupon.findOneAndUpdate({
-                    code: session.metadata.couponCode, 
-                    userId: session.metadata.userId
-                },{
-                    isActive: false
-                })
-            }
-
-            //create a new Order
-            const products = JSON.parse(session.metadata.products);
-            const newOrder = new Order({
-                user: session.metadata.userId,
-                products: products.map(product => ({
-                    product: product.id,
-                    quantity: product.quantity,
-                    price: product.price
-                })),
-                totalAmount: session.amount_total / 100, //Convert from cents to dollars
-                paymentIntent: session.payment_intent,
-                stripeSessionId: sessionId
-            })
-            await newOrder.save()
-            res.status(200).json({
-                success: true,
-                message: "Payment successfull, order created, and coupon deactivated if used",
-                orderId: newOrder._id
-            })
-        }
-    }catch(error){
-        console.error("Error processing successful checkout:",error)
-        res.status(500).json({ message: "Error in processing checkout",error: error.message })
-    }
-})
+router.post("/checkout-success", protectRoute,)
 
 export default router
