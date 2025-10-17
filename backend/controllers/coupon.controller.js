9e9fa87a6141b78687aa704350b567f141f52041
josh098741 <morongemokaya@gmail.com>
2025-10-17 14:32:59 +0300

    Additional commits to validate coupon

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -10,3 +10,29 @@ export const getCoupon = async (req,res) => {
         res.status(500).json({ message: "Internal server error", error: error.message })
     }
 }
+
+export const validateCoupon = async (req,res) => {
+    try{
+        const { code } = req.body
+        const coupon = await Coupon.findOne({ code:code, userId:req.user._id, isActive:true })
+
+        if(!coupon){
+            return res.status(404).json({ message: "Coupon Not Found" })
+        }
+
+        if(coupon.expirationDate < new Date()){
+            coupon.isActive = false
+            await coupon.save()
+            return res.status(404).json({ message: "Coupon expired" })
+        }
+
+        res.json({
+            message: "Coupon is Valid",
+            code: coupon.code,
+            discountPercantage: coupon.discountPercantage,
+        })
+    }catch(error){
+        console.log("Error in validate coupon controller",error.message)
+        res.status(500).json({ message: "Internal server Error",error:error.message })
+    }
+}
