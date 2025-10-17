25c8efbe322526d694e75b739713a4975fbd3a17
josh098741 <morongemokaya@gmail.com>
2025-10-17 14:12:37 +0300

    Additional commit to coupon controller

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,12 @@
+import Coupon from "../models/coupon.model.js"
+
+
+export const getCoupon = async (req,res) => {
+    try{
+        const coupon = await Coupon.findOne({ userId:req.user._id, isActive: true })
+        res.json(coupon || null )
+    }catch(error){
+        console.log("Error in getCoupon controller", error.message)
+        res.status(500).json({ message: "Internal server error", error: error.message })
+    }
+}
