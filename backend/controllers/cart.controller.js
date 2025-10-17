ebb0ca904207c621cc99a92f3c520751e008428c
josh098741 <morongemokaya@gmail.com>
2025-10-17 11:59:20 +0300

    Update quantity

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -49,11 +49,29 @@ export const removeAllFromCart = async (req,res) => {
 
 export const updateQuantity = async (req,res) => {
     try{
-        const { id } = req.params
+        const { id:productId } = req.params
 
         const { quantity } = req.body
-        
-    }catch(error){
 
+        const user = req.user
+
+        const existingItem = user.cartItems.find(item => item.id === productId)
+
+        if(existingItem){
+            if(quantity === 0){
+                user.cartItems = user.cartItems.filter((item) => item.id !== productId)
+                await user.save()
+                return res.status(200).json(user.cartItems)
+            }
+            existingItem.quantity = quantity
+            await user.save()
+            res.status(200).json(user.cartItems)
+        }else{
+            res.status(404).json({ message: "Product Not Found" })
+        }
+
+    }catch(error){
+        console.log("Error in update quantity controller",error.message)
+        res.status(500).json({ message: "Internal Server Error", error: error.message })
     }
 }
