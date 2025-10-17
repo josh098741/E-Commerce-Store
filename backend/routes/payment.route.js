1761509f7477218bab477aa2a52f68ca1952dbdd
josh098741 <morongemokaya@gmail.com>
2025-10-17 18:13:36 +0300

    Cleaning up function

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,9 +1,6 @@
 import express from 'express'
 import { protectRoute } from '../middleware/auth.middleware.js'
 import { createCheckoutSession } from '../controllers/payment.controller.js'
-import { stripe } from '../lib/stripe.js'
-import Coupon from '../models/coupon.model.js'
-import Order from '../models/order.model.js'
 
 const router = express.Router()
 
